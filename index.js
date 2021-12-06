41259ff663debcc6279ea2000a6436ae1f45f8f6
Gabriel Gonzalez Domeniconi <gabrielgonzalezdomeniconi@gmail.com>
2021-12-06 12:10:23 -0300

    added: launch.json for debbuguing, new methods GET, POST, PUT, DELETE, and middlewares

--- index.js
+++ index.js
@@ -1,26 +1,87 @@
-const express = require('express');
+const express = require('express'); //demontra ao programa que vai usar o express
 
-const server = express();
+const server = express(); //inicia o server
 
-//quando o user acessar essa rota exibirá o que está entre chaves;
+server.use(express.json()); //demonstra que o express deve ler json
 
-//query params ----------------------------------------
+//Query Params = ?teste=1
+//Route Params = /users/1
+//Request Body = {"name": "Diego", "email": "email@email.com"}
 
-/* server.get('/teste', (req, res) => { 
-   const nome = req.query.nome;
 
-    return res.json({ message: `Hello ${nome}`})
-}); */
-
-//route params ----------------------------------------
+//array com usuários:
 const users = ['Diego', 'Gabriel', 'Robson'];
 
-server.get('/users/:index', (req, res) => {
-    const {index} = req.params;
+//middleware global:
+server.use((req, res, next) =>{ 
+    console.time('Request');
+    console.log(`Método ${req.method}; URL: ${req.url}`);
 
-    return res.json(users[index]);
+    next();
+    console.timeEnd('Request');
+});
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//middleware local:
+function checkUserExists(req, res, next){ 
+    if(!req.body.name){
+        return res.status(400).json({error: 'Username is required!'})
+    }
+    return next();
+};
+
+function checkUserInArray(req, res, next){
+    const user = users[req.params.index];
+
+    if(!user){
+        return res.status(400).json({error: "Username doesn't exists!"})
+    }
+
+    req.user = user;
+    return next();
+}
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+//methods
+//GET: 1- pega todos os usuários, 2- pega um usuário pelo index usado como parâmetro.
+server.get('/users', (req, res) => {
+    return res.json(users);
 });
 
-//Request Body ----------------------------------------
+server.get('/users/:index', checkUserInArray, (req, res) => {
+
+    return res.json(req.user);
+});
+
+//POST: 1- adiciona um usuário novo.
+server.post('/users', checkUserExists, (req, res) => {
+    const { name } = req.body;
+    users.push(name);
+
+    return res.json(users)
+});
+
+//PUT: 1- altera um usuário existente.
+server.put('/users/:index', checkUserInArray,checkUserExists , (req, res) => {
+    const {index} = req.params;
+    const {name} = req.body;
+
+    users[index] = name;
+
+    return res.json(users);
+});
+
+//DELETE: 1- deleta um usuário existente.
+server.delete('/users/:index', checkUserInArray, (req, res) => {
+    const {index} = req.params;
+
+    users.splice(index, 1);
+    
+    return res.send();
+});
+//=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+
+
+
 
 server.listen(3000);
